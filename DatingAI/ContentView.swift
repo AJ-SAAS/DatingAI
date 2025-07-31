12c4501f5d41a9bbbf55df0076ab7a5e2a6d9298
Ajay Sharma <gridking111@gmail.com>
2025-07-31 18:46:44 +0300

    Initial Commit

---
+++ DatingAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DatingAI
+//
+//  Created by Work Laptop on 31/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DatingAI/DatingAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAIApp.swift
+//  DatingAI
+//
+//  Created by Work Laptop on 31/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct DatingAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
